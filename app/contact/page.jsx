c8866ed58774bb395a9c7d776a503d8ff829f09f
Robiul Islam Sagar <94870525+robiulsagor@users.noreply.github.com>
2024-01-06 11:08:06 +0600

    Contact page done

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,8 +1,20 @@
-
+import Image from "next/image"
+import styles from "./contact.module.css"
 const Contact = () => {
     return (
-        <div>
-            <h2>THis is Contact us page</h2>
+        <div className={styles.container}>
+            <div className={styles.imgContainer}>
+                <Image src="/contact.png" alt="contact" fill />
+            </div>
+            <div className={styles.formContainer}>
+                <form className={styles.form}>
+                    <input type="text" className={styles.input} name="" placeholder="Name and Surname" />
+                    <input type="email" className={styles.input} name="" placeholder="Email" />
+                    <input type="text" className={styles.input} name="" placeholder="Phone Number (optional)" />
+                    <textarea name="" className={styles.textarea} id="" cols="30" rows="10" placeholder="Message"></textarea>
+                    <button className={styles.formSubmit}>Save</button>
+                </form>
+            </div>
         </div>
     )
 }
